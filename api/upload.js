51a8458c55da9bca174c84fc066b89f579b03c07
RIQUELMEDMR <73765375+RIQUELMEDMR@users.noreply.github.com>
2024-06-13 15:18:53 -0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
     try {
         const file = req.files.file;
-        // BLOB_READ_WRITE_TOKEN="vercel_blob_rw_LmkH331Rnpz2jpQ4_zEXp9tTiCdolFfb2emmLwIgsyQaLoA"
+        BLOB_READ_WRITE_TOKEN="vercel_blob_rw_LmkH331Rnpz2jpQ4_zEXp9tTiCdolFfb2emmLwIgsyQaLoA"
         res.status(200).json({ message: 'Upload successful!' });
     } catch (error) {
         console.error('Error uploading file:', error);
